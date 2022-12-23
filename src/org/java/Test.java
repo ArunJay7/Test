6629b8303cd3926be9eef7cd20b6577ead7085d6
Arunj7 <arunjay3003@gmail.com>
2022-12-23 10:18:19 +0530

    first commit

---
+++ src/org/java/Test.java
@@ -0,0 +1,21 @@
+package org.java;
+
+public class Test {
+	  
+	   
+	    public static void main(String[] args) {    
+	        String string = "Java Class";    
+	        int count = 0;    
+	            
+	        //Counts each character except space    
+	        for(int i = 0; i < string.length(); i++) {    
+	            if(string.charAt(i) != ' ')    
+	                count++;    
+	        }    
+	            
+	        //Displays the total number of characters present in the given string    
+	        System.out.println("Total number of characters in a string: " + count);    
+	    }    
+	}     
+
+
